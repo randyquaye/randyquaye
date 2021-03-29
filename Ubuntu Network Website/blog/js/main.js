072f6559de33277a8dd428517d011313fc5c17fc
Randy Quaye <randyquaye@Randys-MacBook-Pro.local>
2021-03-29 09:29:36 +0000

    Current website for Ubuntu Network

---
+++ Ubuntu Network Website/blog/js/main.js
@@ -0,0 +1,66 @@
+(function ($) {
+
+    "use strict";
+    $(".carousel-inner .item:first-child").addClass("active");
+    /* Mobile menu click then remove
+    ==========================*/
+    $(".mainmenu-area #mainmenu li a").on("click", function () {
+        $(".navbar-collapse").removeClass("in");
+    });
+    /* Scroll to top
+    ===================*/
+    $.scrollUp({
+        scrollText: '<i class="zmdi zmdi-long-arrow-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+
+
+
+
+    /*-- Smoth-Scroll --*/
+
+    // Select all links with hashes
+    $('.mainmenu-area a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+
+    /* Preloader Js
+    ===================*/
+    $(window).on("load", function () {
+        $('.preloade').fadeOut(500);
+    });
+})(jQuery);
